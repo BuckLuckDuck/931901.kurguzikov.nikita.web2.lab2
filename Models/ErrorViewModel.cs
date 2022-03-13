47ca16659ea0ccff53754d94d9355ee18b4a282e
BuckLuckDuck <nikitos80011@gmail.com>
2022-03-13 16:48:23 +0700

    Version 1.0

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WEB_Lab_2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
